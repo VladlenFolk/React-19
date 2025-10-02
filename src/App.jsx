7fda65eb56573c790948cfd206b465f2d93d4a63
Avok@to34 <vladlen-17@yandex.ru>
2025-10-02 20:26:14 +0300

    feat: добавил хуки и строку поиска

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
+import { useState } from "react";
 import { MovieCard } from "./MovieCard";
 import { MOVIES } from "./movies.data";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const movies = MOVIES.filter((movie) =>
+    movie.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+  );
+
   return (
     <div className="min-h-screen w-full bg-black text-white px-6 py-5">
-      <header className="mb-10 flex items-center">
+      <header className="mb-10 flex items-center justify-between">
         <img src="/netflix.png" alt="Netflix" className="h-8 w-auto" />
+
+        <input
+          type="search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search..."
+          className="border border-white/15 px-2 py-1 rounded outline-0"
+        />
       </header>
       <main className="flex gap-6">
-        {MOVIES.map((movie) => (
-          <MovieCard
-            key={movie.name}
-            image={movie.image}
-            rating={movie.rating}
-          />
-        ))}
+        {movies.length ? (
+          movies.map((movie) => (
+            <MovieCard
+              key={movie.name}
+              image={movie.image}
+              rating={movie.rating}
+            />
+          ))
+        ) : (
+          <p>Movies not found</p>
+        )}
       </main>
     </div>
   );
