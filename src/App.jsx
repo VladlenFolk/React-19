2cdd33a1f414185c5a2c2e1650ec6d9ae9951409
Avok@to34 <vladlen-17@yandex.ru>
2025-10-02 16:28:42 +0300

    fix: перенес все содержимое из вложенной папки

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { MovieCard } from "./MovieCard";
 function App() {
   return (
     <div className="min-h-screen w-full bg-black text-white px-6 py-5">
+      <header className="mb-10 flex items-center"></header>
       <MovieCard image={"/narcos.jpg"} rating={8.8}  />
     </div>
   );
