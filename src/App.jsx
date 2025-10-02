9999940236424076b4d80a63930899203586ac93
Avok@to34 <vladlen-17@yandex.ru>
2025-10-02 16:37:41 +0300

    feat: добавил классы для компонентов

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import { MovieCard } from "./MovieCard";
 function App() {
   return (
     <div className="min-h-screen w-full bg-black text-white px-6 py-5">
-      <header className="mb-10 flex items-center"></header>
-      <MovieCard image={"/narcos.jpg"} rating={8.8}  />
+      <header className="mb-10 flex items-center">
+        <img src="/netflix.png" alt="Netflix" className="h-8 w-auto" />
+      </header>
+      <main className="flex gap-6">
+        <MovieCard image={"/narcos.jpg"} rating={8.8} />
+      </main>
     </div>
   );
 }
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,10 +1,14 @@
-
-
 export function MovieCard({ image, rating }) {
   return (
-    <div>
-      <div>INDb rating: {rating}</div>
-      <img src={image} alt="" width={200} height={250}/>
+    <div className="relative w-[200px] rounded-2xl overflow-hidden bg-neutral-900 shadow-lg hover:scale-105 transition-transform will-change-transform duration-300">
+      <img
+        src={image}
+        alt="Movie Poster"
+        className="w-full h-auto object-cover"
+      />
+      <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/80 to-transparent p-2 text-sm text-white font-semibold">
+        INDb rating: {rating}
+      </div>
     </div>
   );
 }
